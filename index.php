f9b70c65b3d0a4014261a3706824aa878ddd330d
easygode <bimalbenvenuti@gmail.com>
2023-04-17 10:34:21 +0200

    array of objects

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once_DIR__ . '/Models/Movie.php';
+require_once_DIR __. "/Models/Movie.php";
 
-$supermario = new Movie('Supermario' , 'cartoon', 120, 'tante persone');
+$supermario = new Movie('Supermario' , array(new Genre('cartoon')) new Genre('fantasy'), 120, 'tante persone');
 var_dump ($supermario);
 
 ?>
@@ -17,6 +17,10 @@ var_dump ($supermario);
 </head>
 <body>
     <h1>My Movies</h1>
-    
+    <h2><?php echo $supermario->get_name(); ?></h2>
+    <p>Genre</p>
+    <?php foreach () $supermario->get_name(); ?>
+    <?php echo $supermario->get_name(); ?>
+
 </body>
 </html>
